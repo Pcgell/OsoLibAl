7d2c5d164b2c2806d7178df6b24dc342694c0125
kenneth vittetoe <kenneth.vittetoe@ujcv.edu.hn>
2020-02-26 07:37:28 -0600

    Agregando Vector en R2 (Scafolding o andamio)

---
+++ src/hn/edu/ujcv/progra/OVecR2.java
@@ -0,0 +1,29 @@
+package hn.edu.ujcv.progra;
+
+public class OVecR2 {
+    // miembros
+
+    // metodos
+    public OVecR2 suma(OVecR2 b){
+        //TODO: implementar
+        return new OVecR2();
+    }
+
+    public OVecR2 resta(OVecR2 b){
+        //TODO: implementar
+        return new OVecR2();
+    }
+
+    public double prodPunto(OVecR2 b){
+        //TODO: implementar
+        return 0.0f;
+    }
+
+    
+    // constructores
+    public OVecR2(){}
+
+    public OVecR2(OVecR2 a){}
+
+    public OVecR2(double x, double y){}
+}
