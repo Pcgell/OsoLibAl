39a3e6c116ddd2c77d60286b37e1f9430255cc44
kenneth vittetoe <kenneth.vittetoe@ujcv.edu.hn>
2020-02-28 07:34:35 -0600

    Agregando formula del producto cruz.

--- src/hn/edu/ujcv/progra/OVecR3.java
+++ src/hn/edu/ujcv/progra/OVecR3.java
@@ -6,6 +6,35 @@ public class OVecR3 {
 
 
     // accesoras y mutadoras
+    public OVecR3 suma(OVecR3 b){
+        //TODO: implementar
+        return new OVecR3();
+    }
+
+    public OVecR3 resta(OVecR3 b){
+        //TODO: implementar
+        return new OVecR3();
+    }
+
+    public OVecR3 prodCruz(OVecR3 b){
+        //
+        //  x  y  z
+        //  x1 y1 z1       i = y * z1 - z * y1;
+        //                 j = - ();
+        //                 k = ;
+        //
+        return new OVecR3();
+    }
+
+    public double prodPunto(OVecR3 b){
+        //TODO: implementar
+        return 0.0f;
+    }
+
+    public double magnitud(){
+        //TODO: implementar
+        return 0.0f;
+    }
 
     // metodos
 
