73c67e8cb1691a2c89d48766a145c8a22fa322ec
kenneth vittetoe <kenneth.vittetoe@ujcv.edu.hn>
2020-03-04 08:18:13 -0600

    Agregando los metodos restantes.

--- src/hn/edu/ujcv/progra/OMat4x4.java
+++ src/hn/edu/ujcv/progra/OMat4x4.java
@@ -8,5 +8,67 @@ public class OMat4x4 {
 
     // metodos
 
+    public OMat4x4 transpuesta(){
+        //TODO: implementar
+        return new OMat4x4();
+    }
+
+    public OMat4x4 inversa(){
+        //TODO: implementar
+        return new OMat4x4();
+    }
+    public OMat4x4 suma(OMat4x4 b){
+        //TODO: implementar
+        return new OMat4x4();
+    }
+
+    public OMat4x4 resta(OMat4x4 b){
+        //TODO: implementar
+        return new OMat4x4();
+    }
+
+    public OMat4x4 mult(OMat4x4 b){
+        //TODO: implementar
+        //    m11  m12
+        //    m21  m22
+        //
+        //
+        //  m11   =  a.m11 * b.m11 + a.m12 * b.m21
+        //  m12   =  a.m11 * b.m12 + a.m12 * b.m22
+        //
+        return new OMat4x4();
+    }
+
+    public double determinante(){
+        //TODO: implementar
+        return 0.0f;
+    }
+
+    // Metodos de la clase.
+    public static OMat4x4 rotX(double alpha){
+
+
+        return new OMat4x4();
+    }
+
+    public static OMat4x4 rotY(double alpha){
+
+
+        return new OMat4x4();
+    }
+
+    public static OMat4x4 rotZ(double alpha){
+
+
+        return new OMat4x4();
+    }
+
+    public static OMat4x4 identidad(){
+
+
+        //TODO: implementar
+        return new OMat4x4();
+    }
+
     // constructores
 }
--- src/hn/edu/ujcv/progra/OVecR4.java
+++ src/hn/edu/ujcv/progra/OVecR4.java
@@ -8,5 +8,25 @@ public class OVecR4 {
 
     // metodos
 
+    public OVecR4 suma(OVecR4 b){
+        //TODO: implementar
+        return new OVecR4();
+    }
+
+    public OVecR4 resta(OVecR4 b){
+        //TODO: implementar
+        return new OVecR4();
+    }
+
+    public double prodPunto(OVecR4 b){
+        //TODO: implementar
+        return 0.0f;
+    }
+
+    public double magnitud(){
+        //TODO: implementar
+        return 0.0f;
+    }
+
     // constructores
 }
